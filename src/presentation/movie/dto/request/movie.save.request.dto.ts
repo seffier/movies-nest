01bdf58191006b220ac297a5e6f509677ccc5cb1
Sefer <sefer865@hotmail.com>
2024-02-29 14:02:09 +0300

    validation pipe added for request validation

--- src/presentation/movie/dto/request/movie.save.request.dto.ts
+++ src/presentation/movie/dto/request/movie.save.request.dto.ts
@@ -1,14 +1,53 @@
+import { Type } from 'class-transformer';
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
 export class MovieSaveRequestDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   overview: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
   popularity: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
   voteAverage: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
   voteCount: number;
+
+  @IsDateString()
+  @IsNotEmpty()
   releaseDate: string;
-  genre: genre[];
+
+  @IsNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => GenreDto)
+  genre: GenreDto[];
 }
 
-class genre {
+class GenreDto {
+  @IsNumber()
+  @IsNotEmpty()
   id: number;
+
+  @IsNotEmpty()
+  @IsString()
   name: string;
 }
